ad2bd0c067a193f43b58c22bc971b6a33d30c9d7
Karthik Senthil <karthik.senthil94@gmail.com>
2016-03-01 14:42:53 +0530

    Added theories defined under SMTLIB2

--- src/smt/theories/array_ex.rs
+++ src/smt/theories/array_ex.rs
@@ -1,17 +1,41 @@
 //! Module that describes the ArrayEx Theory
 
+use std::fmt::Debug;
 use std::fmt;
+use smt::smt::SMTNode;
 
+#[macro_use]
+use smt::theories::utils;
+
+#[derive(Clone, Debug)]
 pub enum OpCodes {
     Select,
     Store,
+    FreeVar(String),
 }
 
 impl fmt::Display for OpCodes {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let s = match *self {
-            OpCodes::Select => "select",
-            OpCodes::Store => "store",
+            OpCodes::Select => "select".to_owned(),
+            OpCodes::Store => "store".to_owned(),
+            OpCodes::FreeVar(ref name) => format!("{}", name),
+        };
+        write!(f, "{}", s)
+    }
+}
+
+impl_smt_node!(OpCodes);
+
+#[derive(Clone, Debug)]
+pub enum Sorts {
+    Array(String, String),
+}
+
+impl fmt::Display for Sorts {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match *self {
+            Sorts::Array(ref x, ref y) => format!("(Array {} {})", x, y),
         };
         write!(f, "{}", s)
     }
--- src/smt/theories/core.rs
+++ src/smt/theories/core.rs
@@ -1,9 +1,13 @@
-//! Defines basic operation defined under Core theory in SMTLIB2.
+//! Defines basic operations defined under Core theory in SMTLIB2.
 
 use std::fmt;
 use std::fmt::Debug;
+use smt::smt::SMTNode;
 
-#[derive(Clone, Copy, Debug)]
+#[macro_use]
+use smt::theories::utils;
+
+#[derive(Clone, Debug)]
 pub enum OpCodes {
     True,
     False,
@@ -15,33 +19,37 @@ pub enum OpCodes {
     Cmp,
     Distinct,
     ITE,
+    FreeVar(String),
 }
 
 impl fmt::Display for OpCodes {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let s = match *self {
-            OpCodes::Not => "not",
-            OpCodes::Imply => "=>",
-            OpCodes::And => "and",
-            OpCodes::Or => "or",
-            OpCodes::Xor => "xor",
-            OpCodes::Cmp => "=",
-            OpCodes::Distinct => "distinct",
-            OpCodes::ITE => "ite",
-            OpCodes::True => "true",
-            OpCodes::False => "false",
+            OpCodes::Not => "not".to_owned(),
+            OpCodes::Imply => "=>".to_owned(),
+            OpCodes::And => "and".to_owned(),
+            OpCodes::Or => "or".to_owned(),
+            OpCodes::Xor => "xor".to_owned(),
+            OpCodes::Cmp => "=".to_owned(),
+            OpCodes::Distinct => "distinct".to_owned(),
+            OpCodes::ITE => "ite".to_owned(),
+            OpCodes::True => "true".to_owned(),
+            OpCodes::False => "false".to_owned(),
+            OpCodes::FreeVar(ref name) => format!("{}", name),
         };
         write!(f, "{}", s)
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+impl_smt_node!(OpCodes);
+
+#[derive(Clone, Debug)]
 pub enum Sorts {
     Bool
 }
 
 impl fmt::Display for Sorts {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", "bool")
+        write!(f, "{}", "Bool")
     }
 }
